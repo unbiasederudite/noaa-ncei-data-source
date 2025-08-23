20b6c5907cfa52ad2b858ad177ae76d31e5b597e
Mykhailo Stasiuk <erudite1854@gmail.com>
2025-08-23 21:23:19 +0200

    fix: Fix basic use-case script console output typo

--- scripts/fetch_us_zip_data_csv.py
+++ scripts/fetch_us_zip_data_csv.py
@@ -1,23 +1,23 @@
 from noaa_ncei.api_data.location_data import LocationData
 
-munich_data = LocationData("ZIP:28801")
+us_zip_data = LocationData("ZIP:28801")
 
-print("Fetching Munich Location Datasets data...")
-location_datasets_file_path = munich_data.fetch_location_datasets(format="csv")
-print(f"Munich Location Datasets data saved to {location_datasets_file_path}")
+print("Fetching US ZIP Location Datasets data...")
+location_datasets_file_path = us_zip_data.fetch_location_datasets(format="csv")
+print(f"US ZIP Location Datasets data saved to {location_datasets_file_path}")
 
-print("Fetching Munich Location Categories Data data for the 'GHCND' dataset...")
-location_data_categories_file_path = munich_data.fetch_location_data_categories(format="csv", datasetid="GHCND")
-print(f"Munich Location Categories data saved to {location_data_categories_file_path}")
+print("Fetching US ZIP Location Categories Data data for the 'GHCND' dataset...")
+location_data_categories_file_path = us_zip_data.fetch_location_data_categories(format="csv", datasetid="GHCND")
+print(f"US ZIP Location Categories data saved to {location_data_categories_file_path}")
 
-print("Fetching Munich Location Data Types data for the 'GHCND' dataset...")
-location_data_types_file_path = munich_data.fetch_location_data_types(format="csv", datasetid="GHCND")
-print(f"Munich Location Data Types data saved to {location_data_types_file_path}")
+print("Fetching US ZIP Location Data Types data for the 'GHCND' dataset...")
+location_data_types_file_path = us_zip_data.fetch_location_data_types(format="csv", datasetid="GHCND")
+print(f"US ZIP Location Data Types data saved to {location_data_types_file_path}")
 
-print("Fetching Munich Location Stations data for the 'GHCND' dataset...")
-location_stations_file_path = munich_data.fetch_location_stations(format="csv", datasetid="GHCND")
-print(f"Munich Location Stations data saved to {location_stations_file_path}")
+print("Fetching US ZIP Location Stations data for the 'GHCND' dataset...")
+location_stations_file_path = us_zip_data.fetch_location_stations(format="csv", datasetid="GHCND")
+print(f"US ZIP Location Stations data saved to {location_stations_file_path}")
 
-print("Fetching Munich Location Data from 2025-01-01 to 2025-02-01 for the 'GHCND' dataset...")
-location_data_file_path = munich_data.fetch_location_data(format="csv", datasetid="GHCND", startdate="2010-05-01", enddate="2010-05-01")
-print(f"Munich Location Data saved to {location_data_file_path}")
+print("Fetching US ZIP Location Data from 2010-05-01 to 2010-05-01 for the 'GHCND' dataset...")
+location_data_file_path = us_zip_data.fetch_location_data(format="csv", datasetid="GHCND", startdate="2010-05-01", enddate="2010-05-01")
+print(f"US ZIP Location Data saved to {location_data_file_path}")
